5098d1bf30d2942167fcf068bed4807edf00721e
Phuong Kieu <141943686+kieuphuong2205@users.noreply.github.com>
2025-06-27 23:02:33 +0700

    Update RISCV_Single_Cycle.v

--- Top/RISCV_Single_Cycle.v
+++ Top/RISCV_Single_Cycle.v
@@ -15,6 +15,8 @@ module RISCV_Single_Cycle(clk,rst_n, Instruction_out_top);
 
     input clk,rst_n;
     output [31:0] Instruction_out_top;
+
+    wire rst = ~rst_n;
     wire [31:0] PC_Top,RD_Instr,RD1_Top,Imm_Ext_Top,ALUResult,ReadData,PCPlus4,RD2_Top,SrcB,Result;
     wire RegWrite,MemWrite,ALUSrc;
     wire [1:0]ImmSrc,ResultSrc;
