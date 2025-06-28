8a99c3723e7148870e4db27b459bc0a4873987ac
Phuong Kieu <141943686+kieuphuong2205@users.noreply.github.com>
2025-06-28 10:21:36 +0700

    Create DMEM.v

---
+++ TOP/DMEM.v
@@ -0,0 +1,20 @@
+// --- File: DMEM.v ---
+module DMEM(
+    input         clk,
+    input  [31:0] address,
+    input  [31:0] write_data,
+    input         MemRW,
+    output reg [31:0] read_data
+);
+    reg [31:0] memory [0:1023];
+
+    always @(*) begin
+        read_data = memory[address[11:2]];
+    end
+
+    always @(posedge clk) begin
+        if (MemRW) begin
+            memory[address[11:2]] <= write_data;
+        end
+    end
+endmodule
