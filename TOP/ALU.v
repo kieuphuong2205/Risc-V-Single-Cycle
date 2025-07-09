c146c6e3e2a0758bf2d01904a827dcf269818428
Phuong Kieu <141943686+kieuphuong2205@users.noreply.github.com>
2025-07-09 16:57:43 +0700

    Update ALU.v

--- TOP/ALU.v
+++ TOP/ALU.v
@@ -6,19 +6,19 @@ module ALU(
     input  [3:0]  ALUControl,
     output reg [31:0] Result
 );
-    always @(*) begin
-        case (ALUControl)
-            `ALU_ADD:  Result = A + B;
-            `ALU_SUB:  Result = A - B;
-            `ALU_SLL:  Result = A << B[4:0];
-            `ALU_SLT:  Result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0;
-            `ALU_SLTU: Result = (A < B) ? 32'd1 : 32'd0;
-            `ALU_XOR:  Result = A ^ B;
-            `ALU_SRL:  Result = A >> B[4:0];
-            `ALU_SRA:  Result = $signed(A) >>> B[4:0];
-            `ALU_OR:   Result = A | B;
-            `ALU_AND:  Result = A & B;
-            default:   Result = 32'hdeadbeef;
+    always @(*) begin            // Khối always luôn chạy khi có bất kỳ thay đổi nào ở A, B, hoặc ALUControl
+        case (ALUControl)        // Chọn phép toán dựa trên giá trị ALUControl
+            `ALU_ADD:  Result = A + B;        // Cộng hai số
+            `ALU_SUB:  Result = A - B;        // Trừ A - B
+            `ALU_SLL:  Result = A << B[4:0];  // Dịch trái A theo số bit trong B[4:0]
+            `ALU_SLT:  Result = ($signed(A) < $signed(B)) ? 32'd1 : 32'd0;     //So sánh nhỏ hơn (có dấu)
+            `ALU_SLTU: Result = (A < B) ? 32'd1 : 32'd0;        // So sánh nhỏ hơn (không dấu)
+            `ALU_XOR:  Result = A ^ B;                          // Phép XOR bitwise
+            `ALU_SRL:  Result = A >> B[4:0];                    // Dịch phải (không dấu) A theo B[4:0]
+            `ALU_SRA:  Result = $signed(A) >>> B[4:0];          // Dịch phải (có dấu, bảo toàn bit dấu)
+            `ALU_OR:   Result = A | B;                          // Phép OR bitwise
+            `ALU_AND:  Result = A & B;                          // Phép AND bitwise 
+            default:   Result = 32'hdeadbeef;                   // Mặc định trả về giá trị đặc biệt để dễ debug nếu ALUControl không hợp lệ
         endcase
     end
 endmodule
