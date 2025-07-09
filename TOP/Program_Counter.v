5a7514cc5b9fd69817d2a1c989cb80a47ef925ce
Phuong Kieu <141943686+kieuphuong2205@users.noreply.github.com>
2025-07-09 17:08:34 +0700

    Update Program_Counter.v

--- TOP/Program_Counter.v
+++ TOP/Program_Counter.v
@@ -1,4 +1,3 @@
-// --- File: Program_Counter.v ---
 module Program_Counter(
     input clk,
     input rst_n,
@@ -7,8 +6,8 @@ module Program_Counter(
 );
     always @(posedge clk or negedge rst_n) begin
         if (!rst_n)
-            pc_out <= 32'h00000000;
+            pc_out <= 32'h00000000;     // Khi reset = 0 → đặt PC về 0
         else
-            pc_out <= pc_in;
+            pc_out <= pc_in;            // Ngược lại, cập nhật PC theo giá trị đầu vào
     end
 endmodule
